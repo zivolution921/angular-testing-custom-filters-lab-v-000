76362905c00613d384060c0d2521a9ce54a8d07e
Ziv Zamechek <ziv921@yahoo.com>
2016-08-24 18:45:56 -0400

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,17 @@
 describe('Favorite Food Filter', function () {
-	var $controller;
+  var $controller;
 
-	beforeEach(module('app'));
+  beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
-	}));
+  beforeEach(inject(function ($injector) {
+    $filter = $injector.get('$filter');
+  }));
 
-	
+  it('should filter the foods', function () {
+      var mockedList = [{name: 'dave', favoriteFood: 'pizza'}, {name: 'peter', favoriteFood: 'burger'}]
+      var results = $filter('favoriteFood')(mockedList, 'pizza');
+      expect(results.length).toBe(1);
+      expect(results[0].name).toBe('dave');
+  });
+
 });
